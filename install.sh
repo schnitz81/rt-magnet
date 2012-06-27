245153477492aec9615db69d534c2df6fc8f0ef2
schnitz81 <k.quick81@gmail.com>
2012-06-27 09:37:53 +0200

    Install script add.

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This install script needs to be run as root."; 
+   exit 1
+fi
+
+echo
+echo "Checking for rtorrent config file..."; sleep 1
+echo
+sleep 1 
+if [ -s .rtorrent.rc ]; then
+  echo ".rtorrent.rc found. Installation continues..." ; sleep 1
+else
+  echo "No valid .rtorrent.rc found in this directory! Quitting..."
+  exit
+fi
+
+echo
+echo "Checking for old config file..."; sleep 1
+echo 
+if [ -a /etc/rt-gui.conf ]; then
+  echo "Old config file exists! Do you want to overwrite and generate a new? (y/n)"
+  read CONFIRM
+  case $CONFIRM in
+    y|Y|yes|YES|Yes) echo ;;
+    *)
+      echo " Quitting..."
+      exit ;;
+esac
+  else
+    echo "No old config file found. Creating new."; sleep 1
+fi
+
+
+#echo "DEBUG part"
+echo
+echo "Creating conf-file..."; sleep 1
+echo
+#touch /etc/rt-gui.conf
+echo "# Automatically created by rt-gui installer." > /etc/rt-gui.conf
+cat .rtorrent.rc | grep watch_directory | grep -v '#' >> /etc/rt-gui.conf
+echo "/etc/rt-gui.conf created. Exiting..."
+echo
+exit;
+
