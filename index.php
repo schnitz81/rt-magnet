d4f4b42129ad7ff3a1f559c7815bc7e5fba95540
atquick <at_quick@hotmail.com>
2012-07-02 15:53:41 +0200

    Adds javascript things, through javascript.
    The the user doesn't have javascript activated, they will through this commit
    be able to see the listing of directories at the end of the page.
    
    Rather than assuming that javascript is turned on, this will only add the
    javascript functionality, if javascript is on. If it's not turned on, it will
    fail gracefully, by not adding content that doesn't work.
    
    This commit adds three layers of implementation: Internet Explorer methods,
    W3C methods (pretty much everything except Interet Exporer) and inlining for
    managing the event model. This should capture most javascript implementations,
    unless the browser is really old.

--- index.php
+++ index.php
@@ -1,28 +1,83 @@
 <html>
 <head>
-<script type="textr/javascript">
+<script type="text/javascript">
 // Get the actual CSS value
 function get_style(element, style_property) {
 	if(element.currentStyle) { //Internet Explorer
 		return self.currentStyle[style_property];
 	} else if(window.getComputedStyle) { //Firefox
-		return window.getComputedStyle(element, nil).getPropertyValue(style_property);
+		return window.getComputedStyle(element, null).getPropertyValue(style_property);
 	} else { //Get inlined value
-		return element.style[style_attribute];
+		return element.style[style_property];
         }
 }
-function toggle() {
-	var ele = document.getElementById("toggleText");
-	var text = document.getElementById("displayText");
-	if( get_style(ele, "display") == "block" ) {
-    		ele.style.display = "none";
-		text.innerHTML = "show watch directories...";
-  	}
-	else {
-		ele.style.display = "block";
-		text.innerHTML = "hide";
+// Manage the toggle button, and elements visibility
+// Returns an event handler.
+function bindToggleElements(button, element) {
+	function hidden(elm) {
+		return get_style(elm, "display") == "none";
+	}
+	function hide(elm) {
+		elm.style.display = "none";
+	}
+	function show(elm) {
+		elm.style.display = "block";
+	}
+	return function(event) {
+		if( hidden(element) ) {
+			show(element);
+			button.innerHTML = "Hide text";
+		} else {
+			hide(element);
+			button.innerHTML = "Show text";
+		}
+	};
+}
+// Add a toggle button
+function addShowHideButtons(event) {
+	var button = document.createElement("button");
+	var element= document.getElementById("toggleText");
+	button.innerHTML = "Toggle Visibility";
+	// Add button before element
+	element.parentNode.insertBefore(button, element);
+	// Add events to handle a click, and then fire it to update the button and element
+	// states.
+	if(button.attachEvent) { //Internet Explorer
+		button.attachEvent("onclick", bindToggleElements(button, element));
+		button.fireEvent("onclick", document.createEventObject());
+	} else if(window.addEventListener) { //w3c
+		button.addEventListener("click", bindToggleElements(button, element));
+		e = document.createEvent("HTMLEvents");
+		e.initEvent("click", true, true);
+		button.dispatchEvent(e);
+	} else { //inline
+		// Make sure we don't overwrite existing events by saving them to old_event
+		button.onclick = function(old_event, new_event) {
+			return function(event) {
+				old_event.apply(this, [event]);
+				new_event.apply(this, [event]);
+			}
+		}(button.onclick || function(event) {}, bindToggleElements(button, element))
+		button.onclick();
 	}
 }
+// Add the button when the site has finished loading
+function(context) {
+	if(context.attachEvent) { // windows
+		context.attachEvent("onload", addShowHideButtons);
+	} else if(context.addEventListener) { // W3C
+		context.addEventListener("load", addShowHideButtons);
+	} else { // inline
+		old_onload = context.onload || function(event) {};
+		context.onload = function(old_onload) {
+			return function(event) {
+				addShowHideButtons.apply(this, [event]);
+				old_onload.apply(this, [event]);
+			};
+		}(old_onload);
+	}
+}(window);
+
 </script>
 </head>
 
@@ -95,8 +150,7 @@ echo "<br />";
 </form><br /><br />;
 
 <!------ Show and hide option to display array of watch directories. --> 
-<a id="displayText" href="javascript:toggle();">show watch directories...</a>
-<div id="toggleText" style="display: none"><h3>
+<div id="toggleText"><h3>
 
 <?php
 foreach($arrLinesMasked as $arrItem)
