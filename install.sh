5555a3d2591d7a2233d04f3bd2a1b83939e03f2a
schnitz81 <k.quick81@gmail.com>
2012-09-25 00:11:01 +0300

    Update install.sh
    Added handling of non root and more verbose output when successful.
    Configuration file needs to be reset in order to handle removal of watch dirs.

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ RT_CONFIG=".rtorrent.rc" # Local settings for rtorrent
 UNIQ=true # Force the config file to only contain unique paths
 #VERBOSE=false # not in use
 
-
 CONFIG_DIR=$(dirname $RT_CONFIG)
 if [ "$CONFIG_DIR" == "." ]; then
   CONFIG_DIR=$(pwd)
@@ -21,20 +20,19 @@ fi
 
 # Add all load_start values to the config file
 if [ -s $RT_CONFIG ]; then
-  # First get all non-comments section, extract load_start value, replace ~ 
-  # with the $HOME variable, and dump it in the config file.
-  grep -ohP '^[^#]{1,}' $RT_CONFIG | grep -ohP '(?<=load_start=)[^,]{1,}' | sed -e "s#~#$HOME##" >> $RT_MAGNET_CONFIG
+  if [ -w $RT_MAGNET_CONFIG ]; then
+    # First get all non-comments section, extract load_start value, replace ~ 
+    # with the $HOME variable, and dump it in the config file.
+    grep -ohP '^[^#]{1,}' $RT_CONFIG | grep -ohP '(?<=load_start=)[^,]{1,}' | sed -e "s#~#$HOME##" > $RT_MAGNET_CONFIG
   echo "Config file created."
+  else
+    echo "Can't write to configuration file! Exiting..."
+    exit 1
+  fi
 else
   echo "$RT_CONFIG was not found in this directory!"
   echo "Exiting..."
   exit 1
-fi
-
-# Since we don't reset the file, we may get duplicates, which we can fix if we
-# set the UNIQ flag at the top of the file.
-if [ $UNIQ == true ]; then
-  sort -u $RT_MAGNET_CONFIG -o $RT_MAGNET_CONFIG
 fi
 
 exit 0
