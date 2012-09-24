92990aaf3fd7cfeade9d5e5a980017f4cca5ce08
schnitz81 <k.quick81@gmail.com>
2012-09-25 00:15:21 +0300

    Update index.php
    jscript rewritten to a working version with show/hide-button.

--- index.php
+++ index.php
@@ -16,81 +16,19 @@
 <html>
 <head>
 <script type="text/javascript">
-// Get the actual CSS value
-function get_style(element, style_property) {
-	if(element.currentStyle) { //Internet Explorer
-		return self.currentStyle[style_property];
-	} else if(window.getComputedStyle) { //Firefox
-		return window.getComputedStyle(element, null).getPropertyValue(style_property);
-	} else { //Get inlined value
-		return element.style[style_property];
-        }
+
+function toggleText() {
+   var textElement = document.getElementById("toggleText");
+   var buttonElement = document.getElementById("toggleButton");
+   if(textElement.style.display == "block") {
+      textElement.style.display = "none";
+      buttonElement.value = "Show";
+   }
+   else {
+      textElement.style.display = "block";
+      buttonElement.value = "Hide";
+   }
 }
-// Manage the toggle button, and elements visibility
-// Returns an event handler.
-function bindToggleElements(button, element) {
-	function hidden(elm) {
-		return get_style(elm, "display") == "none";
-	}
-	function hide(elm) {
-		elm.style.display = "none";
-	}
-	function show(elm) {
-		elm.style.display = "block";
-	}
-	return function(event) {
-		if( hidden(element) ) {
-			show(element);
-			button.innerHTML = "Hide text";
-		} else {
-			hide(element);
-			button.innerHTML = "Show text";
-		}
-	};
-}
-// Add a toggle button
-function addShowHideButtons(event) {
-	var button = document.createElement("button");
-	var element= document.getElementById("toggleText");
-	button.innerHTML = "Toggle Visibility";
-	// Add button before element
-	element.parentNode.insertBefore(button, element);
-	// Add events to handle a click, and then fire it to update the button and element
-	// states.
-	if(button.attachEvent) { //Internet Explorer
-		button.attachEvent("onclick", bindToggleElements(button, element));
-		button.fireEvent("onclick", document.createEventObject());
-	} else if(window.addEventListener) { //w3c
-		button.addEventListener("click", bindToggleElements(button, element));
-		e = document.createEvent("HTMLEvents");
-		e.initEvent("click", true, true);
-		button.dispatchEvent(e);
-	} else { //inline
-		// Make sure we don't overwrite existing events by saving them to old_event
-		button.onclick = function(old_event, new_event) {
-			return function(event) {
-				old_event.apply(this, [event]);
-				new_event.apply(this, [event]);
-			}
-		}(button.onclick || function(event) {}, bindToggleElements(button, element))
-		button.onclick();
-	}
-}
-// Add the button when the site has finished loading
-function(context) {
-	if(context.attachEvent) { // windows
-		context.attachEvent("onload", addShowHideButtons);
-	} else if(context.addEventListener) { // W3C
-		context.addEventListener("load", addShowHideButtons);
-	} else { // inline
-		old_onload = context.onload || function(event) {};
-		context.onload = function(old_onload) {
-			return function(event) {
-				addShowHideButtons.apply(this, [event]);
-				old_onload.apply(this, [event]);
-			};
-		}(old_onload);
-}(window);
 
 </script>
 
@@ -173,7 +111,9 @@ function getCategoryName($strPath){ // Extract category names from every watchdi
 <br />
 <br />
 <!------ Show and hide option to display array of watch directories. --> 
-<div id="toggleText">
+<label for="showdirs">Show watch directories:</label>
+<input id="toggleButton" type="button" value="Show" onclick="javascript:toggleText();" />
+<div id="toggleText" style="display: none;">
   <?php
   foreach($directories as $directory)
     echo "  <h3>$directory</h3>\n";
